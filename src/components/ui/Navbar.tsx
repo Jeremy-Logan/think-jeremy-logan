81f09be07c31f3dbb56dcf3097e61f185096e390
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2022-05-29 06:26:23 -0700

    fix: animated logo

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,7 +1,8 @@
 import { motion } from 'framer-motion';
-import Image from 'next/image';
 import Link from 'next/link';
 import { Fragment } from 'react';
+
+import NextImage from '../NextImage';
 
 export default function Navbar() {
   return (
@@ -11,7 +12,7 @@ export default function Navbar() {
           <div className='flex w-full items-center justify-center sm:justify-start'>
             <div className='m-8 h-12 w-12 cursor-pointer sm:h-24 sm:w-24'>
               <Link href='/' passHref>
-                <motion.a>
+                {/* <motion.a>
                   <Image
                     src='/favicon/ms-icon-310x310.png'
                     width='192px'
@@ -19,7 +20,24 @@ export default function Navbar() {
                     alt='logo'
                     layout='responsive'
                   />
-                </motion.a>
+                </motion.a> */}
+                <picture className='h-[192px] w-[192px]'>
+                  <source
+                    srcSet='https://res.cloudinary.com/the-color-mill/image/upload/v1653788491/Color%20Mill%20Design/headerLogo_pi3cib.gif'
+                    type='image/gif'
+                  />
+                  <source
+                    srcSet='https://res.cloudinary.com/the-color-mill/image/upload/v1653830108/Color%20Mill%20Design/TJL-Logo_l5muds.png'
+                    type='image/png'
+                  />
+                  <NextImage
+                    src='https://res.cloudinary.com/the-color-mill/image/upload/v1653788491/Color%20Mill%20Design/headerLogo_pi3cib.gif'
+                    width='192px'
+                    height='192px'
+                    alt='logo'
+                    layout='responsive'
+                  />
+                </picture>
               </Link>
             </div>
             <motion.h1
@@ -28,7 +46,7 @@ export default function Navbar() {
                 y: [0, 0, 0, -25, 0, 0],
               }}
               initial={{ scaleY: 0, y: 0 }}
-              transition={{ delay: 1, duration: 0.3 }}
+              transition={{ delay: 1.2, duration: 0.3 }}
               className='hidden origin-bottom text-2xl font-normal sm:block'
             >
               Think Jeremy Logan
