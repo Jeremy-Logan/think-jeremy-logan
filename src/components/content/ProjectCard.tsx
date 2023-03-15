3d7851c136758633350d5259c1895a8601bee6c2
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2023-03-15 13:39:25 -0700

    fix: typescript errors

--- src/components/content/ProjectCard.tsx
+++ src/components/content/ProjectCard.tsx
@@ -1,12 +1,12 @@
-import { motion } from 'framer-motion';
-import { useInView } from 'framer-motion';
-import Image from 'next/image';
-import { useRef } from 'react';
+import { motion } from "framer-motion";
+import { useInView } from "framer-motion";
+import Image from "next/image";
+import { useRef } from "react";
 
-import 'simplebar-react/dist/simplebar.min.css';
+import "simplebar-react/dist/simplebar.min.css";
 
-import ImageModal from './ImageModal';
-import useToggle from '../hooks/useToggle';
+import ImageModal from "./ImageModal";
+import useToggle from "../hooks/useToggle";
 
 const container = {
   offScreen: { opacity: 0 },
@@ -21,40 +21,41 @@ const container = {
   },
 };
 
-interface Props {
+type Props = {
   props: {
-    section: string;
-    projects: {
+    section?: string;
+    projects?: {
       title: string;
       content: {
-        imageURL: string;
         link: string;
+        imageURL: string;
       };
     }[];
-  };
-}
-
+  }
+};
 
 const ProjectCard = ({ props }: Props) => {
   const [toggleIsOpen] = useToggle(false);
   const ref = useRef(null);
   const inView = useInView(ref, { amount: 0.2 });
 
+  const { projects, section } = props;
+
   return (
     <div
       ref={ref}
-      className='mt-0 flex w-full flex-col items-center justify-center lg:mt-24 '
+      className="mt-0 flex w-full flex-col items-center justify-center lg:mt-24 "
     >
-      <h2 className='mt-12 mb-4 text-center text-4xl font-thin text-white lg:mb-12'>
-        {props.section}
+      <h2 className="mt-12 mb-4 text-center text-4xl font-thin text-white lg:mb-12">
+        {section && section}
       </h2>
-      <div className='z-10 mx-auto mt-2 mb-20 h-[1px] w-[50vw] bg-white'></div>
+      <div className="z-10 mx-auto mt-2 mb-20 h-[1px] w-[50vw] bg-white"></div>
 
-      <motion.div className='my-auto mx-auto grid h-full w-full grid-cols-1 gap-4 md:w-[65vw] md:grid-cols-2'>
-        {props.projects.map((project, i) => {
+      <motion.div className="my-auto mx-auto grid h-full w-full grid-cols-1 gap-4 md:w-[65vw] md:grid-cols-2">
+        {projects && projects.map((project, i) => {
           return project.content.link ? (
             <motion.div
-              initial='offScreen'
+              initial="offScreen"
               animate={
                 inView
                   ? {
@@ -62,36 +63,36 @@ const ProjectCard = ({ props }: Props) => {
                       scale: 1,
                       transition: { duration: 0.3, delay: i * 0.2 },
                     }
-                  : 'offScreen'
+                  : "offScreen"
               }
               variants={container.variants}
-              className='flex flex-col justify-center'
+              className="flex flex-col justify-center"
               key={i}
             >
               <a
                 href={project.content.link}
-                target='_blank'
-                rel='noopener noreferrer'
-                className='transtion-all mx-auto h-[90vw] w-[90vw] shadow-lg duration-300 ease-in-out hover:scale-105 sm:h-[70vw] sm:w-[70vw] md:h-[30vw] md:w-[30vw] '
+                target="_blank"
+                rel="noopener noreferrer"
+                className="transtion-all mx-auto h-[90vw] w-[90vw] shadow-lg duration-300 ease-in-out hover:scale-105 sm:h-[70vw] sm:w-[70vw] md:h-[30vw] md:w-[30vw] "
               >
-                <button className='relative z-0 h-full w-full'>
+                <button className="relative z-0 h-full w-full">
                   <Image
                     src={project.content.imageURL}
                     alt={project.title}
-                    fill = {true}
-          style={{objectFit: 'contain'}}
-          sizes="(max-width: 768px) 45vw,"
+                    fill={true}
+                    style={{ objectFit: "contain" }}
+                    sizes="(max-width: 768px) 45vw,"
                   />
                 </button>
               </a>
-              <div className='mx-auto mt-6 h-[1px] w-36 bg-white lg:w-72' />
-              <h3 className='mt-2 mb-12 self-center text-center font-light text-white'>
+              <div className="mx-auto mt-6 h-[1px] w-36 bg-white lg:w-72" />
+              <h3 className="mt-2 mb-12 self-center text-center font-light text-white">
                 {project.title}
               </h3>
             </motion.div>
           ) : (
             <motion.div
-              initial='offScreen'
+              initial="offScreen"
               animate={
                 inView
                   ? {
@@ -99,17 +100,17 @@ const ProjectCard = ({ props }: Props) => {
                       scale: 1,
                       transition: { duration: 0.3, delay: i * 0.2 },
                     }
-                  : 'offScreen'
+                  : "offScreen"
               }
               variants={container.variants}
-              className='flex flex-col justify-center'
+              className="flex flex-col justify-center"
               key={i}
             >
-              <div className='transtion-all mx-auto h-[90vw] w-[90vw] shadow-lg duration-300 ease-in-out hover:scale-105 sm:h-[70vw] sm:w-[70vw] md:h-[30vw] md:w-[30vw] '>
+              <div className="transtion-all mx-auto h-[90vw] w-[90vw] shadow-lg duration-300 ease-in-out hover:scale-105 sm:h-[70vw] sm:w-[70vw] md:h-[30vw] md:w-[30vw] ">
                 <ImageModal project={project} isOpen={toggleIsOpen} />
               </div>
-              <div className='mx-auto mt-6 h-[1px] w-36 bg-white lg:w-72' />
-              <h3 className='mt-2 mb-12 self-center text-center font-light text-white'>
+              <div className="mx-auto mt-6 h-[1px] w-36 bg-white lg:w-72" />
+              <h3 className="mt-2 mb-12 self-center text-center font-light text-white">
                 {project.title}
               </h3>
             </motion.div>
--- src/components/sections/ProjectSection.tsx
+++ src/components/sections/ProjectSection.tsx
@@ -1,16 +1,18 @@
-import * as React from 'react';
+import * as React from "react";
 
-import ProjectCard from '../content/ProjectCard';
-import projects from '../../lib/projectData.json';
+import ProjectCard from "../content/ProjectCard";
+import projects from "../../lib/projectData.json";
 
 const ProjectSection = () => {
   return (
-    <div className='z-50 mt-60 w-screen border-t-[1px] border-t-indigo-600 bg-indigo-900/90'>
-      <ProjectCard props={projects.projectData[0]} key={0} />
-      <ProjectCard props={projects.projectData[1]} key={1} />
-      <ProjectCard props={projects.projectData[2]} key={2} />
+    <div className="z-50 mt-60 w-screen border-t-[1px] border-t-indigo-600 bg-indigo-900/90">
+      {projects.projectData.map((project, index) =>
+        project && <ProjectCard props={project} key={index} />
+      )}
     </div>
   );
 };
 
 export default ProjectSection;
+
+
